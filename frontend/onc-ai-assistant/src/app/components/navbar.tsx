90859baf4d4dd7a68da689ec55004092b0f69825
TMunro7 <taymunro@uvic.ca>
2025-05-21 13:45:40 -0700

    Nav Bar Updates

--- frontend/onc-ai-assistant/src/app/components/navbar.tsx
+++ frontend/onc-ai-assistant/src/app/components/navbar.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import "./navbar.css"
 
 export default function Navbar() {
   return (
     <nav className="navBar">
-      <h1>ONC AI Assistant</h1>
-      <div>
+      <Link href="https://www.oceannetworks.ca/" className="logo">
+        <img src="/ONC_Logo.png" alt="ONC Logo" className="h-20 w-auto" />
+      </Link>
+      <div className="navLinks">
         <Link href="/">Home</Link>
         <Link href="/chatPage">Chat</Link>
         <Link href="/adminPages">Admin</Link>
+        <Link
+          // href="/signin"   uncomment this line when sign in page is ready
+          href="/"
+          className="sign-in-btn"
+        >
+          Sign in
+        </Link>
       </div>
     </nav>
   );
