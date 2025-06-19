29ac4648714746b1e57245cb178d17cbfbcd5fc8
Treesa Sabu <treesasabu@uvic.ca>
2025-06-19 15:24:08 -0700

    Added a function for dowmloaidng files from ONC

--- src/database_search/get_data_generalized.py
+++ src/database_search/get_data_generalized.py
@@ -2,6 +2,8 @@ import requests
 from datetime import datetime, timedelta, timezone
 import time
 import json
+import os
+
 
 API_TOKEN = "b77b663d-e93b-40a3-a653-dfccb4a1b0cb"
 BASE_URL = "https://data.oceannetworks.ca/api"
@@ -147,8 +149,169 @@ def get_cambridge_bay_sensor_data(
         traceback.print_exc()
         return f"Error: {e}"
 
+
+#Fucntion to download files:
+def get_cambridge_bay_data_products( location_code, 
+        relevant_device_catagory_code, 
+        relevant_property_code,
+        relevant_dataproduct_name,
+        extension,
+        start_time,
+        end_time
+        ):
+    
+    start_total = time.time()
+    if start_time == None:
+        start_time = end_time - timedelta(days=1)
+
+    try:
+        # get data product code 
+        step1_start = time.time()
+        devices_product_discovery = requests.get(         #dataProducts discovery API call to get dataProduct code and extension 
+            f"{BASE_URL}/dataProducts",
+            params={  
+                'token': API_TOKEN,
+                'locationCode': location_code,
+                'deviceCategoryCode':relevant_device_catagory_code,
+                'propertyCode':relevant_property_code,
+            },
+            timeout=30
+        )
+        if devices_product_discovery.status_code != 200:
+            return f"Error getting data Products: HTTP {devices_product_discovery.status_code}"
+        
+        relevant_data_products = devices_product_discovery.json()
+        step1_time = time.time() - step1_start
+    
+        data_products = [d for d in relevant_data_products if d.get('dataProductName') == relevant_dataproduct_name and d.get('extension') == extension]
+
+        if not data_products:
+            total_time = time.time() - start_total
+            print(f"Total time: {total_time:.2f}s")
+            return f"No {relevant_dataproduct_name} found in {relevant_device_catagory_code}"
+        
+        #looping through each data product code 
+        for data_product in data_products:
+            data_product_code = data_product['dataProductCode']
+            extension= data_product['extension']
+
+             # get most recent data from device (last 10 minutes)
+            step2_start = time.time()
+            
+            # print(f"Time range: {start_time} to {end_time}")
+            print(f"data product code: {data_product_code} & extension: {extension}")
+
+            #API call to request dataProduct delivery and retrive request ID from the response 
+            request_dataproduct = requests.get(   
+            f"{BASE_URL}/dataProductDelivery",
+            params={
+           'token': API_TOKEN,
+           'method': 'request',  
+           'locationCode': location_code,
+           'deviceCategoryCode': relevant_device_catagory_code,
+           'propertyCode': relevant_property_code,
+           'dataProductCode': data_product_code,
+           'extension': extension,
+           'dateFrom': start_time,
+           'dateTo': end_time,
+            },
+            timeout=30
+            )
+            dataproduct_request_details = request_dataproduct.json()
+            requestID = dataproduct_request_details.get("dpRequestId")
+         
+            if requestID:
+               print(f"requestID: {requestID}")
+            else:
+               print(f"no requestID found{dataproduct_request_details}")
+               continue
+
+            status = 'none'
+            runID = None
+            file_count = None
+            while status != 'complete':
+                #API call to run data Product devilery and retrive runID, status, and filecount
+                run_dataproduct = requests.get(   
+                f"{BASE_URL}/dataProductDelivery",
+                params={
+                'token': API_TOKEN,
+                'method': 'run',   
+                'dpRequestId': requestID
+                },
+                timeout=30
+                )
+                dataproduct_run_details = run_dataproduct.json()
+
+                first_run = dataproduct_run_details[0]
+                status = first_run.get("status")
+                runID = first_run.get("dpRunId")
+                file_count = first_run.get("fileCount")
+
+                print(f"Status: {status}, runID: {runID}, fileCount: {file_count}")
+
+                if status == 'complete' and runID:
+                  print(f"runID: {runID}")                   #print runID when then file is ready to download
+                elif status in ['error', 'cancelled']:    
+                  return f"Data product request failed or was cancelled: {dataproduct_run_details}"
+                else:
+                  time.sleep(10)  # wait before next poll
+
+            
+            
+            base_name = f"{location_code}_{data_product_code}_{runID}"
+            for file_index in range(1, file_count+1):
+                while True:
+                    print(f"Downloading file {file_index} of {file_count}...")
+                    #API call to download the file 
+                    download_dataproduct = requests.get(
+                         f"{BASE_URL}/dataProductDelivery",
+                         params={
+                         'token': API_TOKEN,
+                         'method': 'download',
+                         'dpRunId': runID,
+                         'index': file_index
+                         },
+                        timeout=60  
+                    )
+                    if  download_dataproduct.status_code == 202:
+                        print(f"File {file_index} not ready yet. Retrying...")
+                        time.sleep(10)                     
+                    elif download_dataproduct.status_code == 200:
+                        filename = f"{base_name}_file{file_index}.{extension}"
+                        with open(filename, 'wb') as f:
+                            f.write(download_dataproduct.content)
+                        print(f"File saved: {os.path.abspath(filename)}")
+                        break  # move to next file
+                    else:            
+                        print(f"Error downloading file index {file_index}: HTTP {download_dataproduct.status_code}")
+                        print(f"Response: {download_dataproduct.text}")
+                        break ## move to next file
+                  
+        
+        total_time = time.time() - start_total
+        print(f"Total time: {total_time:.2f}s")
+        
+            
+            
+    except Exception as e:
+        total_time = time.time() - start_total
+        print(f"Total time (with error): {total_time:.2f}s")
+        print(f"Exception details: {type(e).__name__}: {e}")
+        import traceback
+        traceback.print_exc()
+        return f"Error: {e}"
+    
+
+
 if __name__ == "__main__":
     end_time = datetime(2024, 1, 11, tzinfo=timezone.utc)
+
+    # default values: row_limit = 1 and resample_period= 1sec
+    # if we want hourly data for 24 hours: row_limit = 24 and resample_period= 3600sec
+    # if we want daily data for a month: row_limit = 31 and resample_period= 86400sec
+    #Row_limit = 31
+    #resample_Period = 86400 
+
     # print("Getting temperature from Cambridge Bay CTD sensors...")
     result = get_cambridge_bay_sensor_data(
         location_code='CBYSS.M2', 
@@ -162,5 +325,17 @@ if __name__ == "__main__":
         relevant_device_catagory_code='VIDEOCAM', 
         relevant_property_code='focus',
         # end_time=end_time
+        )
+    
+    print(f"{result}\n") 
+    
+    result = get_cambridge_bay_data_products(
+        location_code='CBYIP', 
+        relevant_device_catagory_code='CTD', 
+        relevant_property_code='depth',
+        relevant_dataproduct_name= 'Time Series Scalar Data',
+        extension= 'txt',
+        start_time='2025-01-31T00:00:00.000Z',
+        end_time= '2025-01-31T23:59:59.000Z'
         )
     print(f"{result}\n")
