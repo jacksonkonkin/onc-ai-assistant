d37ffdbed1254c668261787947e37458eda5ba6e
lisaundre <lisaundre@gmail.com>
2025-06-21 12:50:19 -0700

    set up modules for three main features and added file picker to upload section

--- frontend/onc-ai-assistant/src/app/adminPages/page.tsx
+++ frontend/onc-ai-assistant/src/app/adminPages/page.tsx
@@ -1,8 +1,35 @@
+'use client';
+
+import './adminPanel.css';
+
 export default function AdminPage() {
+
   return (
-    <div>
-      <main>
-        <h2>Admin Page</h2>
+    <div className="admin-container">
+      <h1>Admin Dashboard</h1>
+
+      <main className="admin-dashboard">
+        <div className="dash-column">
+          <div className="module">
+            <h2>View Analytics</h2>
+          </div>
+        </div>
+        <div className="dash-column">
+          <div className="module">
+            <h2>Review User Feedback & Frequent Queries</h2>
+          </div>
+
+          <div className="module">
+            <h2>Upload Documents</h2>
+            <div className="upload"> 
+              <p><i>Drag and drop/upload files here to enhance the assistant's model.</i></p>
+              <label>
+                Browse
+                <input type="file" id="file" style={{display: 'none'}}/>
+              </label>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
