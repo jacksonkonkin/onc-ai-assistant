1beb025e064d9f21874c671e809d2a7304086a8d
jacksonkonkin <jacksonkonkin@gmail.com>
2025-07-29 13:07:12 -0700

    fixes

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import api
 from app.routes import auth
-from app.routes import chat_history  # import chat routes
-from app.routes import message_routes  # import message routes
+from app.routes import chat_history
+from app.routes import message_routes
 
 origins = [
     "http://localhost:3000",
@@ -21,11 +21,9 @@ app.add_middleware(
 
 app.include_router(api.router)
 app.include_router(auth.router)
+app.include_router(chat_history.router)
+app.include_router(message_routes.router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-app.include_router(chat_history.router)     # include chat_history router
-app.include_router(message_routes.router)   # include messages router
